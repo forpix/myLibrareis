fd8ee6a61b91547ce09da363058cd9144689f435
mdali_1 <32926705+forpix@users.noreply.github.com>
2019-10-16 16:29:28 +0530

    created the function

--- vars/createuser.groovy
+++ vars/createuser.groovy
@@ -1,9 +1,11 @@
-import jenkins.model.*
-import hudson.security.*
+def user () {
+  import jenkins.model.*
+  import hudson.security.*
 
-def instance = Jenkins.getInstance()
+  def instance = Jenkins.getInstance()
 
-// Create a jenkins user
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("jenkinsadmin","password1234")
-instance.setSecurityRealm(hudsonRealm)
+  // Create a jenkins user
+  def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+  hudsonRealm.createAccount("jenkinsadmin","password1234")
+  instance.setSecurityRealm(hudsonRealm)
+}
