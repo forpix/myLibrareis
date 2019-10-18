2b5716b365ce132a699d7862edfe9f0479f46ab7
mdali_1 <32926705+forpix@users.noreply.github.com>
2019-10-18 22:41:26 +0530

    buildAndTest functions were added

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -2,33 +2,40 @@ def call(){
     //Jenkinsfile
 node {
   stage('build') {
-    //build code (ex: mvn deploy)
+    echo 'from build stage'
   }
   stage('deploy to qa') {
     //deploy code (ex: aws cloudformation create-stack)
+      echo 'from deploy to qa stage'
   }
   stage('test') {
     //test code (ex: sh /path/to/test/script.sh)
+      echo 'from build stage'
   }
   stage('deploy to staging') {
     //deploy code (ex: aws cloudformation create-stack)
+      echo 'deploy to staging'
   }
   stage('test') {
     //test code (ex: sh /path/to/test/script.sh)
+      echo 'from test stage'
+      packageArtifact()
   }
   stage('deploy to prod') {
     //deploy code (ex: aws cloudformation create/update-stack)
+      echo 'from build deploy to prod stage'
+      buildAndTest()
       }
-    }
+   }
 }
 def packageArtifact(){
     stage("Package artifact") {
-        sh "mvn package"
+        sh "date"
     }
 }
 
 def buildAndTest(){
     stage("Backend tests"){
-        sh "mvn test"
+        sh "pwd"
     }
 }
