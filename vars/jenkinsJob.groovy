541dbda301d29c5d03a5c7cac590a0df574d4084
mdali_1 <32926705+forpix@users.noreply.github.com>
2019-10-18 22:32:31 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,16 +1,24 @@
 def call(){
-    node {
-        stage('Checkout') {
-            checkout scm
-        }
-
-        // Execute different stages depending on the job
-        if(env.JOB_NAME.contains("deploy")){
-            packageArtifact()
-        } else if(env.JOB_NAME.contains("test")) {
-            buildAndTest()
-        }
-    }
+    //Jenkinsfile
+node {
+  stage('build') {
+    //build code (ex: mvn deploy)
+  }
+  stage('deploy to qa') {
+    //deploy code (ex: aws cloudformation create-stack)
+  }
+  stage('test') {
+    //test code (ex: sh /path/to/test/script.sh)
+  }
+  stage('deploy to staging') {
+    //deploy code (ex: aws cloudformation create-stack)
+  }
+  stage('test') {
+    //test code (ex: sh /path/to/test/script.sh)
+  }
+  stage('deploy to prod') {
+    //deploy code (ex: aws cloudformation create/update-stack)
+  }
 }
 
 def packageArtifact(){
