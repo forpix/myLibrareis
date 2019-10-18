c95340cfc69280211a617d6fc56ea2900ca71b81
mdali_1 <32926705+forpix@users.noreply.github.com>
2019-10-18 22:22:46 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -1,33 +1,7 @@
 def call(){
-
-deleteDir()
     node {
         stage('Checkout') {
             checkout scm
-        
-    try {
-        stage ('Build') {
-            sh "echo 'shell scripts to build project...'"
-           
-        }
-        stage ('Tests') {
-            parallel 'static': {
-                sh "echo 'shell scripts to run static tests...'"
-            },
-            'unit': {
-                sh "echo 'shell scripts to run unit tests...'"
-            },
-            'integration': {
-                sh "echo 'shell scripts to run integration tests...'"
-            }
-        }
-        stage ('Deploy') {
-            sh "echo 'shell scripts to deploy to server...'"
-        }
-    } catch (err) {
-        currentBuild.result = 'FAILED'
-        throw err
-    }
         }
 
         // Execute different stages depending on the job
@@ -39,10 +13,9 @@ deleteDir()
     }
 }
 
-def build(){
+def packageArtifact(){
     stage("Package artifact") {
-        def mvnHome = tool name: 'maven 3.5.4', type: 'maven'
-        sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
+        sh "mvn package"
     }
 }
 
